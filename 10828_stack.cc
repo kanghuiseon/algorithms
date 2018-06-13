ec93787fc5c5c2e430bfdef6c343f30808d9c78b
kanghuiseon <ekfsla0113@naver.com>
2018-06-13 15:01:40 +0900

    add more code

--- 10828_stack.cc
+++ 10828_stack.cc
@@ -1,1 +1,37 @@
 #include <iostream>
+#include <string>
+void push(){
+
+}
+int pop(){
+
+}
+int size(){
+
+}
+int empty(){
+
+}
+int top(){
+
+}
+
+int main(){
+  #count : count of a loop, num : the number to enter the stack
+  int count, num;
+  string command;
+  cin >> count;
+  for(int i=0; i<count; i++){
+    cin >> command;
+    cin >> num;
+    switch (command) {
+      case push:
+        push();
+      case pop:
+        pop();
+
+    }
+  }
+
+
+}
