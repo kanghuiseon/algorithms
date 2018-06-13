d1673923b9766111b965a9537fda212925e87425
kanghuiseon <ekfsla0113@naver.com>
2018-06-14 02:00:46 +0900

    final 10828_stack.cc

--- 10828_stack.cc
+++ 10828_stack.cc
@@ -1,57 +1,66 @@
 #include <iostream>
-#include <stack>
+#include <stdio.h>
 #include <string>
+
 using namespace std;
+int list[10000];
 int top = -1;
-void push(int* list, int n){
+
+void push(int n){
   list[++top]=n;
 }
-void empty(){
+int empty(){
   if(top == -1){
-    cout << "1" << endl;
+    return 1;
   }
   else{
-    cout << "0" << endl;
+    return 0;
   }
 }
-int pop(int* list){
+void pop(){
   if(empty()==1){
     cout << "-1" << endl;
   }
-  cout << stack[top] << endl;
-  list[top--] = 0;
+  else{
+    cout << list[top] << endl;
+    list[top--] = 0;
 }
-int size(int* list){
-  cout << list.size() << endl;
 }
-
-int top(){
-  cout << top << endl;
+void size(){
+  cout << top+1 << endl;
 }
 
 int main(){
-  #count : count of a loop, num : the number to enter the stack
+  //count : count of a loop, num : the number to enter the stack
   int count, num;
-  int *list = new int;
   string command;
   cin >> count;
   for(int i=0; i<count; i++){
     cin >> command;
-    cin >> num;
-    switch (command) {
-      case push:
-        push(list);
-      case pop:
-        pop(list);
-      case top:
-        top();
-      case size:
-        size(list);
-      case empty:
-        empty();
+
+    if(command == "push"){
+      cin >> num;
+      push(num);
+    }
+    else if(command == "empty"){
+      cout << empty() << endl;
+    }
+    else if(command == "pop"){
+      pop();
+    }
+    else if(command == "size"){
+      size();
+    }
+    else if(command == "top"){
+      if(empty()==1)
+        cout<< "-1" << endl;
+      else{
+        cout << list[top] << endl;
+      }
+    }
+    else{
+      break;
     }
   }
-  delete list;
-
-
+  return 0;
 }
