934408aa6238b87f1a71cedadc6fb1245887eb3c
kanghuiseon <ekfsla0113@naver.com>
2018-06-13 18:46:10 +0900

    add more code

--- 10828_stack.cc
+++ 10828_stack.cc
@@ -1,24 +1,38 @@
 #include <iostream>
+#include <stack>
 #include <string>
-void push(){
-
+using namespace std;
+int top = -1;
+void push(int* list, int n){
+  list[++top]=n;
 }
-int pop(){
-
+void empty(){
+  if(top == -1){
+    cout << "1" << endl;
+  }
+  else{
+    cout << "0" << endl;
+  }
 }
-int size(){
-
+int pop(int* list){
+  if(empty()==1){
+    cout << "-1" << endl;
+  }
+  cout << stack[top] << endl;
+  list[top--] = 0;
 }
-int empty(){
-
+int size(int* list){
+  cout << list.size() << endl;
 }
+
 int top(){
-
+  cout << top << endl;
 }
 
 int main(){
   #count : count of a loop, num : the number to enter the stack
   int count, num;
+  int *list = new int;
   string command;
   cin >> count;
   for(int i=0; i<count; i++){
@@ -26,12 +40,18 @@ int main(){
     cin >> num;
     switch (command) {
       case push:
-        push();
+        push(list);
       case pop:
-        pop();
-
+        pop(list);
+      case top:
+        top();
+      case size:
+        size(list);
+      case empty:
+        empty();
     }
   }
+  delete list;
 
 
 }
