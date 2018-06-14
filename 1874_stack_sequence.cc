15467acf33dfdef5f22edb88b083e0643f9e17c3
kanghuiseon <ekfsla0113@naver.com>
2018-06-15 02:22:51 +0900

    add more code in 1874_stack_sequence.cc

--- 1874_stack_sequence.cc
+++ 1874_stack_sequence.cc
@@ -1,6 +1,36 @@
 #include <iostream>
 #include <stack>
+using namespace std;
 
 int main(){
-  
+  //k is for index of list
+  //j is for index of n
+  int count, num, k=0, j=0;
+  int list[10000];
+  stack<int> n;
+  cin >> count;
+  //entering the input number in list stack
+  for(int i=0; i<count; i++){
+    cin >> num;
+    list[i] = num;
+  }
+  for(int i=0; i<count; i++){
+    n.push(j+1);
+    cout<<"+"<<endl;
+    while(1){
+      int a = n.top();
+      //cout << a << endl;
+      //cout << list[k] << endl;
+      if(list[k] == a){
+        n.pop();
+        cout << "-" << endl;
+        k++;
+      }
+      else{
+        break;
+      }
+    }
+    j++;
+  }
+  return 0;
 }
