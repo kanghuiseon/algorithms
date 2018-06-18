852c5125138d311a6c73cc5d20e205595d9328b1
kanghuiseon <ekfsla0113@naver.com>
2018-06-19 01:47:35 +0900

    add more code

---
+++ 9012_parentheses.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+int main()
+{
+  int cnt;
+  string par;
+  cin >> cnt;
+  for(int i=0; i<cnt; i++){
+    cin >> par;
+  }
+  return 0;
+}
