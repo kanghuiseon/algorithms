a4e14f0e5174c6fbd081424252a74604f8c0bf10
kanghuiseon <ekfsla0113@naver.com>
2018-06-20 02:20:21 +0900

    final_9012_parentheses.cc

--- 9012_parentheses.cc
+++ 9012_parentheses.cc
@@ -7,9 +7,27 @@ int main()
 {
   int cnt;
   string par;
+  stack<char> stack;
   cin >> cnt;
   for(int i=0; i<cnt; i++){
-    cin >> par;
+      cin >> par;
+      for(int j=0; j<par.length(); j++){
+        if(par[j] =='(' || stack.empty()){
+          stack.push(par[j]);
+        }
+        else if(stack.top() == '('){
+          stack.pop();
+        }
+      }
+      if(stack.empty()){
+        cout << "YES" << endl;
+      }
+      else{
+        cout << "NO" << endl;
+      }
+      while(!stack.empty()){
+        stack.pop();
+      }
   }
   return 0;
 }
