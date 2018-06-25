cb989ce4b0e2c5d9b0df6186a155f0142bbf0819
kanghuiseon <ekfsla0113@naver.com>
2018-06-25 16:08:10 +0900

    add more code at 10845

--- 10845_queue.cc
+++ 10845_queue.cc
@@ -16,6 +16,12 @@ int size(){
 int empty(){
 
 }
+int front(){
+
+}
+int back(){
+
+}
 
 int main(){
   int cnt, num;
@@ -25,11 +31,25 @@ int main(){
     cin >> cmd;
     if(cmd == "push"){
       cin >> num;
-      queue[front++] = num;
+      push(num);
     }
     else if(cmd == "pop"){
-
+      pop();
     }
-
+    else if(cme == "size"){
+      size();
+    }
+    else if(cmd == "empty"){
+      empty();
+    }
+    else if(cmd == "front"){
+      front();
+    }
+    else if(cmd == "back"){
+      back();
+    }
+    else{
+      return 0;
+    }
   }
 }
