03506eb5065429f23b2d25db73c759c6e0e1360f
kanghuiseon <ekfsla0113@naver.com>
2018-07-25 18:38:56 +0900

    add more code at 1260

--- 1260_BFS_DFS.cc
+++ 1260_BFS_DFS.cc
@@ -1,12 +1,28 @@
 #include <iostream>
+#include <stack> //for DFS
+#include <queue> //for BFS
+#include <vector>
+#include <algorithm> //for fill function
 using namespace std;
+
 int main(){
-  int v, e, n1;
-  cin >> v;
-  cin >> e;
-  cin >> n1;
-  for(int i=0; i<e; i++){
-    cin >>
+  int n, m, start;
+  cin >> n >> m >> start;
+  vector<int> list[n+1];
+  bool check[n+1];
+  fill(check, check+n+1, false);
+  for(int i = 0; i<m; i++){
+    int u, v;
+    cin >> u >> v;
+
+    list[u].push_back(v);
+    list[v].push_back(u);
   }
+  for(int i=1; i<=n; i++){
+    sort(list[i].begin(), list[i].end());
+  }
+
+  dfs(start, list, check);
+  printf("\n");
   return 0;
 }
