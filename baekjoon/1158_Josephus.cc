5650fcd2144a453019160c237168bb0aa5d65cc7
kanghuiseon <ekfsla0113@naver.com>
2018-09-04 19:17:49 +0900

    study_180904

--- baekjoon/1158_Josephus.cc
+++ baekjoon/1158_Josephus.cc
@@ -8,15 +8,48 @@ typedef struct Node{
 
 node *head;
 
-void insert(int N){
+void insert_node(int N){
   node *t;
   int i;
   t = (node*)malloc(sizeof(node));
   t->data = 1;
-  he
+  head = t;
+
   for(i=2; i<=N; i++){
     t->next = (node*)malloc(sizeof(node));
-
+    t->next->data = i;
+    t = t->next;
   }
+  t->next = head;
+}
 
+void delete_node(node *t){
+  node* del_node;
+  del_node = t->next;
+  t->next = t->next->next;
+  free(del_node);
+}
+
+void josephus(int N, int M){
+  node *t;
+  int i;
+  insert_node(N);
+  t = head;
+  while(t != t->next){
+    for(i=0; i<M-1; i++){
+      t = t->next;
+    }
+    printf("%d, ",t->next->data);
+    delete_node(t);
+  }
+  printf("%d",t->data);
+}
+
+int main(){
+  int n,m;
+  scanf("%d %d",&n,&m);
+  printf("<");
+  josephus(n,m);
+  printf(">\n");
+  return 0;
 }
