576ab7786d42e81325aed7ba1aeb8564d50f0909
Kangheeseon <ekfsla0113@naver.com>
2021-08-03 19:51:08 +0900

    Create 12738+가장긴증가하는부분수열3.cpp

---
+++ baekjoon/골드/12738+가장긴증가하는부분수열3.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#define MAX_INDEX 10000000
+int arr[MAX_INDEX];
+using namespace std;
+int main(){
+    int n;
+    scanf("%d", &n);
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+    vector<int> lis;
+    
+    for(int i=0; i<n; i++){
+        if(lis.empty()){
+            lis.push_back(arr[i]);
+            continue;
+        }
+        int last = lis.back();
+        if(last < arr[i]){
+            lis.push_back(arr[i]);
+            continue;
+        }
+        int index = lower_bound(lis.begin(), lis.end(), arr[i]) - lis.begin();
+        lis[index] = arr[i];
+    }
+    int s = lis.size();
+    printf("%d\n", s);
+    return 0;
+}
