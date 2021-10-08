ac7c0cea809e146c0df0aa3cbafa3946a72e46f2
Kangheeseon <ekfsla0113@naver.com>
2021-10-08 17:42:26 +0900

    Create 2109+순회강연.cpp

---
+++ baekjoon/골드/2109+순회강연.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<pair<int, int>> v;
+    priority_queue<int, vector<int>, greater<int>> pq;
+    for(int i=0; i<n; i++){
+        int p, d;
+        cin >> p >> d;
+        v.push_back(make_pair(d, p));
+    }
+    sort(v.begin(), v.end());
+    for(int i=0; i<v.size(); i++){
+        int day = v[i].first;
+        pq.push(v[i].second);
+        if(pq.size() > day){
+            pq.pop();
+        }
+    }
+    int sum = 0;
+    while(!pq.empty()){
+        sum += pq.top();
+        pq.pop();
+    }
+    printf("%d\n", sum);
+    return 0;
+}
+
