02d4edc4c449c578fa80ff5da5668ae3def8dbf7
Kangheeseon <ekfsla0113@naver.com>
2021-12-06 19:35:48 +0900

    Update JumpGame.cpp

--- leetCode/JumpGame.cpp
+++ leetCode/JumpGame.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int cnt = 0;
+        int Max = 0;
         for(int i=0; i<nums.size(); i++){
-            if(cnt < i){
+            // i번째까지도 못갔다는 말임
+            // Max 가 i보다 작다는거는 i전까지의 인덱스에서 i까지 도달할 수 있는 인덱스가 하나도 없었다는 말
+            if(Max < i){
                 return false;
             }
-            cnt = max(cnt, i+nums[i]);
+            Max = max(Max, i+nums[i]);
         }
         return true;
     }
